98b626781035dad868eeabc0176e2fbc2846024a
Junior SA <juniorsanni527@gmail.com>
2026-04-25 12:35:22 +0100

    Features: Review Departement function

--- app/Http/Controllers/DepartementController.php
+++ app/Http/Controllers/DepartementController.php
@@ -4,23 +4,78 @@ namespace App\Http\Controllers;
 
 use App\Models\Departement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class DepartementController extends Controller
 {
+    //Page pour afficher la liste de departements
     public function index()
     {
         $departements = Departement::all();
-        return view('auth.departments.show',['departements'=>$departements]);
+
+        return view('auth.departments.show', ['departements' => $departements]);
     }
 
+    //Page pour ajouter un departement
     public function create()
     {
         return view('auth.departments.create');
     }
 
+    //Page pour editer un departement
+    public function edit($id)
+    {
+        $departement = Departement::find($id);
+        if ($departement === null) {
+            abort(404);
+        }
+        return view('auth.departments.edit', ['departement' => $departement]);
+    }
+
+    //Function pour ajouter un departement dans la base de donnee
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|unique:departements|max:255',
+            'description' => 'nullable',
+            'status' => 'required|in:actif,inactif',
+        ], [
+            'title.required' => 'Titre est requis',
+            'title.unique' => 'Titre doit etre unique',
+            'status.required' => 'Statut est requis',
+            'status.in' => 'Statut doit etre actif ou inactif',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->route('departements.create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
         Departement::create($request->all());
+
+        Alert::success('Département ajouté avec succès');
+        return redirect()->route('departements.index');
+    }
+
+    //Function pour modifier un departement
+    public function update(Request $request, $id)
+    {
+        $departement = Departement::find($id);
+        $departement->update($request->all());
+
+        Alert::success('Département modifié avec succès');
+
+        return redirect()->route('departements.index');
+    }
+
+    //Function pour supprimer un departement
+    public function destroy($id)
+    {
+        $departement = Departement::find($id);
+        $departement->delete();
+        Alert::success('Département supprimé avec succès');
         return redirect()->route('departements.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 // PAGE LOGIN
 Route::get('/', function () {
-return view('login');
+    return view('login');
 });
 
 
@@ -16,7 +16,6 @@ Route::post('login', [UserController::class, 'login'])->name('login');
 // Logout
 Route::post('logout', [UserController::class, 'logout'])->name('logout');
 
-<<<<<<< HEAD
 //Page utilisateur
 Route::get('/users', function () {
     return view('auth.utilisateurs.users');
@@ -26,7 +25,7 @@ Route::get('/users', function () {
 Route::get('/users', [UserController::class, 'index'])->name('users');
 
 //register
-Route::post('/register', [UserController::class,'store'])->name('register');
+Route::post('/register', [UserController::class, 'store'])->name('register');
 
 Route::get('/users/edit/{id}', [UserController::class, 'edit'])->name('users.edit');
 
@@ -35,41 +34,39 @@ Route::post('/users/update/{id}', [UserController::class, 'update'])->name('user
 Route::post('/users/delete/{id}', [UserController::class, 'destroy'])->name('users.delete');
 
 //Page Dashboard
-=======
 
 // DASHBOARD
->>>>>>> 783f9d8731a60689c938e51003d64c02926fd9a1
 Route::get('/dashboard', function () {
-return view('auth.dashboard');
+    return view('auth.dashboard');
 })->name('dashboard');
 
 // DEPARTEMENTS
 Route::prefix('departements')->group(function () {
-Route::get('/', [DepartementController::class, 'index'])->name('departements.index');
-Route::get('/create', [DepartementController::class, 'create'])->name('!!!!');
-Route::post('/store', [DepartementController::class, 'store'])->name('departements.store');
-Route::get('/edit/{id}', [DepartementController::class, 'edit'])->name('departements.edit');
-Route::post('/update/{id}', [DepartementController::class, 'update'])->name('departements.update');
-Route::delete('/delete/{id}', [DepartementController::class, 'destroy'])->name('departements.destroy');
-Route::get('/show/{id}', [DepartementController::class, 'show'])->name('departements.show');
+    Route::get('/', [DepartementController::class, 'index'])->name('departements.index');
+    Route::get('/create', [DepartementController::class, 'create'])->name('departements.create');
+    Route::post('/store', [DepartementController::class, 'store'])->name('departements.store');
+    Route::get('/edit/{id}', [DepartementController::class, 'edit'])->name('departements.edit');
+    Route::post('/update/{id}', [DepartementController::class, 'update'])->name('departements.update');
+    Route::delete('/delete/{id}', [DepartementController::class, 'destroy'])->name('departements.destroy');
+    Route::get('/show/{id}', [DepartementController::class, 'show'])->name('departements.show');
 });
 
 // FINANCES
 Route::prefix('finances')->group(function () {
 
-Route::get('/', function () {
-    return view('auth.finances.show');
-})->name('finances.show');
+    Route::get('/', function () {
+        return view('auth.finances.show');
+    })->name('finances.show');
 
-Route::get('/create', function () {
-    return view('auth.finances.create');
-})->name('finances.create');
+    Route::get('/create', function () {
+        return view('auth.finances.create');
+    })->name('finances.create');
 
-Route::get('/update/{id}', function ($id) {
-    return view('auth.finances.update');
-})->name('finances.update');
+    Route::get('/update/{id}', function ($id) {
+        return view('auth.finances.update');
+    })->name('finances.update');
 
-Route::get('/delete/{id}', function ($id) {
-    return 1;
-})->name('finances.delete');
+    Route::get('/delete/{id}', function ($id) {
+        return 1;
+    })->name('finances.delete');
 });
